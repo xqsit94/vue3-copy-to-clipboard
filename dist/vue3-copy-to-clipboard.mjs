14908b191beae549c7f14ce2b96ae6cc9b71ecbb
Manikandan B <xqsit94@gmail.com>
2023-09-08 13:00:16 +0530

    Optimized

--- dist/vue3-copy-to-clipboard.mjs
+++ dist/vue3-copy-to-clipboard.mjs
@@ -1,62 +1,55 @@
-import { defineComponent as c, openBlock as l, createBlock as s, resolveDynamicComponent as d, withModifiers as C, withCtx as y, renderSlot as u } from "vue";
-import b from "copy-to-clipboard";
+import { defineComponent as p, openBlock as i, createBlock as s, resolveDynamicComponent as C, withModifiers as d, withCtx as y, renderSlot as b } from "vue";
+import u from "copy-to-clipboard";
 function n() {
   return {
     copyToClipboard: (r) => {
       try {
-        b(r);
+        u(r);
       } catch {
         throw new Error("Unable to copy");
       }
     }
   };
 }
-const f = /* @__PURE__ */ c({
+const T = /* @__PURE__ */ p({
   __name: "CopyToClipboard",
   props: {
-    as: {
-      required: !1,
-      type: String,
-      default: "button"
-    },
-    copy: {
-      required: !0,
-      type: String
-    }
+    as: { default: "button" },
+    copy: null
   },
   emits: ["success", "error"],
   setup(o, { emit: r }) {
-    const t = o;
-    c({
+    const c = o;
+    p({
       name: "CopyToClipboard"
     });
-    const a = () => {
-      const { copyToClipboard: e } = n();
+    const l = () => {
+      const { copyToClipboard: t } = n();
       try {
-        e(t.copy), r("success");
-      } catch (p) {
-        r("error", p);
+        t(c.copy), r("success");
+      } catch (e) {
+        r("error", e);
       }
     };
-    return (e, p) => (l(), s(d(o.as), {
-      onClick: C(a, ["prevent"])
+    return (t, e) => (i(), s(C(o.as), {
+      onClick: d(l, ["prevent"])
     }, {
       default: y(() => [
-        u(e.$slots, "default")
+        b(t.$slots, "default")
       ]),
       _: 3
     }, 8, ["onClick"]));
   }
-}), { copyToClipboard: i } = n(), h = {
+}), { copyToClipboard: a } = n(), h = {
   install(o) {
-    o.config.globalProperties.$copyToClipboard = i, o.component("CopyToClipboard", f);
+    o.config.globalProperties.$copyToClipboard = a, o.component("CopyToClipboard", T);
   }
 };
-function g(o) {
-  return i(o);
+function k(o) {
+  return a(o);
 }
 export {
-  g as $copyToClipboard,
-  f as CopyToClipboard,
+  k as $copyToClipboard,
+  T as CopyToClipboard,
   h as default
 };
