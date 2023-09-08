2e32b95b57b32a4dce3bb724f88e98f45e6593e5
Manikandan B <xqsit94@gmail.com>
2023-09-08 13:04:19 +0530

    Optimized
    * Optimized
    
    * Update README.md

--- src/components/CopyToClipboard.vue
+++ src/components/CopyToClipboard.vue
@@ -1,28 +1,32 @@
 <script setup lang="ts">
-import { defineComponent, defineProps } from 'vue'
+import { defineComponent } from 'vue'
 import { useCopyToClipboard } from '../use/useCopyToClipboard.js'
+
 defineComponent({
   name: 'CopyToClipboard',
 })
-const prop = defineProps({
-  as: {
-    required: false,
-    type: String,
-    default: 'button',
-  },
-  copy: {
-    required: true,
-    type: String,
-  },
+
+interface Props {
+  as?: string
+  copy: string
+}
+
+const prop = withDefaults(defineProps<Props>(), {
+  as: 'button',
 })
-const emit = defineEmits(['success', 'error'])
+
+const emit = defineEmits<{
+  (event: 'success'): void
+  (event: 'error', error: Error): void
+}>()
+
 const copyToClipboard = () => {
   const { copyToClipboard } = useCopyToClipboard()
   try {
     copyToClipboard(prop.copy)
     emit('success')
   } catch (error) {
-    emit('error', error)
+    emit('error', error as Error)
   }
 }
 </script>
